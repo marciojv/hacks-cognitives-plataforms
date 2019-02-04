2f75266499b60ad80a67307ae7e2635e9e2101d5
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2019-02-03 23:08:41 -0200

    alterado index.py

--- docker/index.py
+++ docker/index.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def hello():
     return "Olá Mundo!"
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=int("5000"), debug=True)
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
