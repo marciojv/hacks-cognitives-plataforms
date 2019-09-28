a49b38f748d2f54f71b8251627fe2936f690378a
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2019-09-28 12:27:35 -0300

    enxugado arquivos de reconhecimento facial

--- opensource/opencv/capturafaces.py
+++ opensource/opencv/capturafaces.py
@@ -1,25 +1,31 @@
 import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_default.xml')
-#cap = cv2.VideoCapture('datasets/videos/professor.mp4')
 cap = cv2.VideoCapture(0)
 
 amostra = 1
 numAmostras = 25
-idFace = raw_input('Seu Nome:')
+id = raw_input('Seu Nome:')
+largura, altura = 220,220 # padronizacao do tamanho das imagens
+
+print "Capturando Faces..."
 
 while(True):
 	conectado, imagem = cap.read()
- 	imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BRG2GRAY)
-	facesDetectadas = face_cascadedetectMultiScale(imagemCinza,scaleFactor=1.5,minSize=(100,100))
+ 	imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(imagemCinza,scaleFactor=1.5,minSize=(150,150))
 
-	for (x,y,w,h) in faces:
-		img = cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
+	for (x,y,l,a) in faces:
+		cv2.rectangle(imagem,(x,y),(x+l,y+a),(255,0,0),2)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			imagemFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura,altura))
+			cv2.imwrite("results/alunos/aluno."+str(id)+"."+str(amostra)+".jpg",imagemFace)
+			print ("foto " + str(amostra) + " capturada")
+			amostra += 1
 
-	cv2.imshow('frame',img)
-
-	key = cv2.waitKey(1)   # fica monutorando teclas clicadas , 1 he milisegundos de espera
-	if key == 27:  # aborta se for a tela ESC
+	cv2.imshow('Face',imagem)
+	cv2.waitKey(1)
+	if (amostra >= numAmostras +1):
 		break
 
 cap.release()
