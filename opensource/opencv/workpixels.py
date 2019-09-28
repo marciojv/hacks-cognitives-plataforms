9f8a178ca9c3b772e45518aeb61e9cf8458a29cc
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2019-09-27 22:07:39 -0300

    Adicionado exemplo para trabalhar com pixel

--- opensource/opencv/workpixels.py
+++ opensource/opencv/workpixels.py
@@ -4,22 +4,24 @@ import cv2
 imagem = cv2.imread("datasets/fotos/reuniao-professores.jpeg")
 
 print imagem.shape
-# imagem em BRG ( de 0 a 2 )
-     # Canal Vermelho 2 , Canal Verde 1    , Canal Azul 0 
+# Imagem em BRG ( de 0 a 2 )
+#      Canal Vermelho 2 , Canal Verde 1    , Canal Azul 0 
 print imagem.item(0,0,2),imagem.item(0,0,1),imagem.item(0,0,0)
 
-print imagem.shape[0]
+alturaImagem =  imagem.shape[0]
 
+# Adicionando um Pixel 
 #imagem.itemset((0,0,2),255)
 #imagem.itemset((0,0,1),0)
 #imagem.itemset((0,0,0),0)
 
-
-
-#posicaopixel = 0
-#for posicaopixel <= imagem.item(0,0,2)
+# Adiconando linha na cor vermelha com 1 pixel de largura
+posicaopixel = 0
+while ( posicaopixel < alturaImagem ):
+	imagem.itemset((posicaopixel,0,2),255)
+	imagem.itemset((posicaopixel,0,1),0)
+	imagem.itemset((posicaopixel,0,0),0)
+	posicaopixel= posicaopixel+1
   
-
-
-#cv2.imwrite("results/imagemvermelha.jpeg",imagem)
+cv2.imwrite("results/imagemvermelha.jpeg",imagem)
 
